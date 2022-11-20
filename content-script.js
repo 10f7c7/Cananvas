895615c153e0120dcc991dc85f4fb664a21813a8
10f7c7 <10f7c7@gmail.com>
2022-11-19 21:41:22 -0500

    mid way thru adding better TODO, trnsfr to dekstop

--- content-script.js
+++ content-script.js
@@ -1,3 +1,5 @@
+// "use strict";
+
 //changePage();
 
 
@@ -71,9 +73,9 @@ var id = window.location.href;
 // };
 
 function setGrade()  {
-    if (id == "https://hcpss.instructure.com" || id == "https://hcpss.instructure.com/" || id == "https://hcpss.instructure.com/#homeroom")  {
+    if (id == "https://hcpss.instructure.com" || id == "https://hcpss.instructure.com/" || id == "https://hcpss.instructure.com/#homeroom" || id == "https://hcpss.instructure.com/#dashboard-activity")  {
         var courseCard = document.getElementsByClassName("ic-DashboardCard");
-        var menuBtn = document.getElementsByClassName("ic-DashboardCard__header-button");     
+        var menuBtn = document.getElementsByClassName("ic-DashboardCard__header-button");
         var observer = new MutationObserver(addGrade);
         observer.observe(document.getElementById("DashboardCard_Container"), { childList: true, subtree: true });
         function addGrade()  {
@@ -82,7 +84,7 @@ function setGrade()  {
                 var crsnm = courseCard[i].children[0].children[2].href;
                 var crsnmstr = crsnm[38] + crsnm[39] + crsnm[40] + crsnm[41] + crsnm[42] + crsnm[43];
                 var div = document.createElement('DIV');
-                div.setAttribute("style", "z-index: 10; position: absolute; background-color: white; height: 22px; padding-right: 5px; padding-left: 5px; margin-top: 10px; margin-right: 10px; margin-left: 10px; float: left; border-radius: 10px; text-align: center;");
+                div.setAttribute("style", `z-index: 10; position: absolute; background-color: white; height: 22px; padding-right: 5px; padding-left: 5px; margin-top: 10px; margin-right: 10px; margin-left: 10px; float: left; border-radius: 10px; text-align: center; color: ${courseCard[i].children[0].children[1].children[0].style.backgroundColor}`);
                 div.innerHTML = "N/A";
                 div.setAttribute("onclick", "window.open('https://hcpss.instructure.com/courses/" + crsnmstr + "/grades'); event.stopPropagation()");
                 courseCard[i].children[0].insertBefore(div, courseCard[i].children[0].children[0]);
@@ -107,20 +109,20 @@ function setGrade()  {
                             endTime = endTime.slice(0,10);
                             // console.log("endtime" + endTime);
 
-                            currentDate = new Date();   
+                            let currentDate = new Date();
 
-                            startDate = new Date(currentDate.getTime());
+                            let startDate = new Date(currentDate.getTime());
                             startDate.setYear(startTime.split("-")[0]);
                             startDate.setMonth(startTime.split("-")[1]-1);
                             startDate.setDate(startTime.split("-")[2]);
                             // console.log("startdate"+startDate);
-                            endDate = new Date(currentDate.getTime());
+                            let endDate = new Date(currentDate.getTime());
                             endDate.setYear(endTime.split("-")[0]);
                             endDate.setMonth(endTime.split("-")[1]-1);
                             endDate.setDate(endTime.split("-")[2]);
                             // console.log("endDate" + endDate);
 
-                            valid = startDate <= currentDate && endDate > currentDate;
+                            let valid = startDate <= currentDate && endDate > currentDate;
                             // console.log(valid)
                             // console.log("current" + currentDate);
                             if (valid == true) {
@@ -161,5 +163,90 @@ function setAsgmtBckBtn()  {
 }
 
 function setBetterTODO()  {
-    console.log("test");
+    if (id == "https://hcpss.instructure.com" || id == "https://hcpss.instructure.com/" || id == "https://hcpss.instructure.com/#homeroom" || id == "https://hcpss.instructure.com/#dashboard-activity")  {
+
+        function waitForElm(selector) {
+            return new Promise(resolve => {
+                if (document.querySelector(selector)) {
+                    return resolve(document.querySelector(selector));
+                }
+        
+                const observer = new MutationObserver(mutations => {
+                    if (document.querySelector(selector)) {
+                        resolve(document.querySelector(selector));
+                        observer.disconnect();
+                    }
+                });
+        
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        }
+        var observer = new MutationObserver(addTODO);
+        observer.observe(document.getElementById('right-side'), { childList: true, subtree: true, attributes: true });
+        async function addTODO()  {
+            observer.disconnect();
+            console.log("test");
+            var todoButton = document.createElement('button');
+            todoButton.style.float = "right";
+            todoButton.type = 'button';
+            todoButton.value = 'false';
+            todoButton.onclick = toggleTODO;
+            todoButton.innerText = `Toggle TODOS: ${todoButton.value}`;
+
+            
+            var listContainer = document.createElement('ul');
+            listContainer.setAttribute('class', 'fOyUs_bGBk fOyUs_UeJS fClCc_bGBk fClCc_fLbg');
+            await fetch('https://hcpss.instructure.com/api/v1/users/self/todo', {method:"GET"})
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+            // https://du11hjcvx0uqb.cloudfront.net/dist/webpack-production/94237-c-bf251b2a9c.js
+
+
+            // document.getElementById('planner-todosidebar-item-list').parentElement
+
+
+
+            function toggleTODO()  {
+                todoButton.value = todoButton.value == 'true' ? 'false' : 'true';
+                todoButton.innerText = `Toggle TODOS: ${todoButton.value}`;
+                insertTODO(todoButton);
+                // if (todoButton.value == 'false') {
+                //     todoButton.value = 'true';
+                //     todoButton.innerText = `Toggle TODOS: ${todoButton.value}`;
+                //     return
+                // }
+                // if (todoButton.value == 'true') {
+                //     todoButton.value = 'false';
+                //     todoButton.innerText = `Toggle TODOS: ${todoButton.value}`;
+                //     return
+                // }
+            }
+
+            await waitForElm('.todo-list-header').then((elm) => elm.appendChild(todoButton));
+
+            const btnAppend = await waitForElm('.ToDoSidebarItem').then(() => {
+                var todoHeader = document.getElementsByClassName('todo-list-header')[0];
+                todoHeader.appendChild(todoButton);
+                insertTODO(todoButton);
+            })
+
+            async function insertTODO(todoButton)  {
+                if (todoButton.value == 'true')  {
+                    document.getElementById('planner-todosidebar-item-list').style.display = "none";
+                }
+                if (todoButton.value == 'false')  {
+                    document.getElementById('planner-todosidebar-item-list').style.display = "block";
+
+                }
+            }
+
+
+
+
+        }
+
+    }
 }
